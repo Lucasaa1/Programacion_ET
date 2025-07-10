9ba406720287e914b914305090d6f40935013f2e
Lucasaa1 <luc.saa@duocuc.cl>
2025-07-10 17:29:52 -0400

    Add files via upload

--- ET_LucasSaa.py
+++ ET_LucasSaa.py
@@ -1,3 +1,28 @@
+# Diccionario Productos
+productos = {
+    "8475HD": ["HP", 15.6, "8GB", "DD", "1T", "Intel Core i5", "Nvidia GTX1050"],
+    "2175HD": ["Acer", 14, "4GB", "SSD", "512GB", "Intel Core i5", "Nvidia GTX1050"],
+    "JjfFHD": ["Asus", 14, "16GB", "SSD", "256GB", "Intel Core i7", "Nvidia RTX2080Ti"],
+    "fgdxHD": ["HP", 15.6, "12GB", "DD", "1T", "Intel Core i3", "integrada"],
+    "GF75HD": ["Dell", 15.6, "8GB", "DD", "1T", "AMD Ryzen 3", "Nvidia GTX1050"],
+    "123FHD": ["Acer", 14, "6GB", "DD", "1T", "AMD Ryzen 5", "integrada"],
+    "342FHD": ["Acer", 15.6, "8GB", "DD", "1T", "AMD Ryzen 7", "Nvidia GTX1050"],
+    "UWU131HD": ["Dell", 15.6, "8GB", "DD", "1T", "AMD Ryzen 3", "Nvidia GTX1050"],
+    "FS1230HD": ["Dell", 15.6, "8GB", "DD", "1T", "AMD Ryzen 3", "Nvidia GTX1050"],
+}
+
+# Diccionario Stock
+stock = {
+    "8475HD": [387990, 10],
+    "2175HD": [327990, 4],
+    "JjfFHD": [424990, 1],
+    "fgdxHD": [664990, 21],
+    "123FHD": [290890, 32],
+    "342FHD": [444990, 7],
+    "GF75HD": [749990, 2],
+    "UWU131HD": [349990, 1],
+    "FS1230HD": [249990, 0],
+}
 
 def stock_marca(marca):
     encontrado = False
